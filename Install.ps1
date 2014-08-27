d5ebefc843ee1f20f48e14b7991f02e8bd1ed305
Daniel Little <lavinski@live.com.au>
2014-08-27 16:12:08 +1000

    Fixed Installer syntax error

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,10 @@
-$files = (
-	@{ Name = "$Profile", Url = "https://raw.githubusercontent.com/Lavinski/PowerShellProfile/master/Profile.ps1" }
+$files = @(
+	@{ Name = "$Profile"; Url = "https://raw.githubusercontent.com/Lavinski/PowerShellProfile/master/Profile.ps1" }
 )
 
 $files | % {
 	(new-object net.webclient).DownloadString($_.Url) | Set-Content $_.Name -Force
 }
+
+# Reload Profile
+. $Profile
